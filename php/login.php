475995ddf41adb17f85721f4b3ce21f4c9154fef
ZhongWwwHhh <79801664+ZhongWwwHhh@users.noreply.github.com>
2022-11-01 21:31:12 +0800

    add

--- php/login.php
+++ php/login.php
@@ -1,20 +1,49 @@
-<!DOCTYPE html>
-<html lang="zh-cn">
-
 <?php
-$uid=$_POST["uid"];
-if (!$uid==null) {
-    $url=$uid;
+$uid = $_POST["uid"];
+
+// unexpected uid
+if ($uid == null) {
+    header("Location:420.html");
+}
+
+// information for mysql
+$host = 'localhost';
+$db_username = 'mysql';
+$db_pwd = 'mysql';
+$db_name = 'yxg3pan';
+// start connect to mysql db
+$conn = mysqli_connect($host, $db_username, $db_pwd, $db_name);
+
+if (!$conn) {
+    header("Location:500.html");
 } else {
-    $url="/404";
+    // connect mysql success
+    $check_query = mysqli_query($conn, "select id from user where lightname='$uid'");
+    $arr = mysqli_fetch_assoc($check_query);
+
+    if ($arr) {
+        // login success
+        // start session
+        session_start();
+        $time = 120; // 2 minute timeout
+        // set cookie
+        setcookie(session_name(), session_id(), time() + $time, "/");
+        // redirect
+        header("Location:/html/file.html");
+    } else {
+        // login fail no user
+        header("Location:/html/login.html");
+    }
 }
 ?>
+
+<!DOCTYPE html>
+<html lang="zh-cn">
 
 <head>
     <meta charset="UTF-8">
     <title>Logining</title>
     <link rel="stylesheet" type="text/css" href="/css/loading.css">
-    <meta http-equiv="refresh" content="1;url=/file/<?php echo $url; ?>">  
 </head>
 
 <body>
@@ -28,4 +57,3 @@ if (!$uid==null) {
 </body>
 
 </html>
-
