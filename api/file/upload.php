f443d34fc434a3f1371bb9ea2603e2a753f92dc6
ZhongWwwHhh <79801664+ZhongWwwHhh@users.noreply.github.com>
2023-01-07 14:49:08 +0800

    add api upload

---
+++ api/file/upload.php
@@ -0,0 +1,106 @@
+<?php
+/* use POST to send fileName & blobNum & totalBlobNum
+ * back: bad, continue, fail, success
+ * warn: login first, provide correct information
+ * !!!: should prevent attack
+ */
+header('Content-Type:text/plain;charset=utf8');
+
+function bad()
+{
+    echo 'bad';
+    exit;
+}
+
+// POST input check
+($_POST['filename'] && $_POST['blobnum'] && $_POST['totalblobnum']) || bad();
+(is_numeric($_POST['blobnum']) && is_numeric($_POST['totalblobnum'])) || bad();
+(strlen($_POST['filename']) <= 25 && $_POST['totalblobnum'] <= 200 && $_POST['blobnum'] <= $_POST['totalblobnum']) || bad();
+
+// session
+require_once '../../function/session.php';
+sessionStart();
+
+($_SESSION['lightname'] && $_SESSION['write']) || (session_destroy()) . (header('Location:/')) . (exit);
+
+// file size, less than 1MB
+$_FILES['myFile']['size'] <= 1048576 || bad();
+
+
+$filepath = $_SESSION['filepath'];
+$fileName = $_POST['filename'];
+$blobNum = $_POST['blobnum'];
+$totalBlobNum = $_POST['totalblobnum'];
+$upDir = '../../../storage/' . $filepath . '/up/';
+$upFileDir = $upDir . $fileName . '_';
+$upFile = $upFileDir . $blobNum;
+
+session_write_close();
+
+// move from tmp to uploadpath
+if (!is_dir($upDir)) {
+    mkdir($upDir);
+}
+move_uploaded_file($_FILES['myFile']['tmp_name'], $upFile);
+
+if ($blobNum >= $totalBlobNum) {
+    // the last blob
+
+    function fail()
+    {
+        require_once '../../function/file.php';
+        delDir($upDir);
+        echo 'fail';
+        exit;
+    }
+
+    // marge
+    // can't open
+    if (!$outFile = @fopen('../../../storage/' . $filepath . '/' . date('YmdHis') . $fileName, "wb")) {
+        fail();
+    }
+
+    // try to marge
+    // lock
+    if (flock($outFile, LOCK_EX)) {
+
+        for ($i = 1; $i <= $totalBlobNum; $i++) {
+            $in = $upFileDir . $i;
+
+            // check blob exist and readable
+            if (!file_exists($in) || !$inFile = @fopen($in, "rb")) {
+                flock($outFile, LOCK_UN);
+                fail();
+            }
+
+            // write
+            while ($buff = fread($inFile, 4096)) {
+                fwrite($outFile, $buff);
+            }
+
+            // close and delete
+            @fclose($inFile);
+            @unlink($in);
+        }
+
+
+        // unlock
+        flock($outFile, LOCK_UN);
+    } else {
+        fail();
+    }
+
+    // write done, close
+    @fclose($outFile);
+
+    // delete upDir
+    require_once '../../function/file.php';
+    delDir($upDir);
+
+    echo 'success';
+    exit;
+} else {
+    // need next blob
+    echo 'continue';
+    exit;
+}
---
+++ function/session.php
@@ -0,0 +1,77 @@
+<?php
+
+// start session, with time control
+function sessionStart(bool $reset = false)
+{
+    // lifetime should not change
+    $lifeTime = 36000;
+
+    // cookie configuration
+    $sessionCookie = array('lifetime' => $lifeTime, 'path' => '/', 'httponly' => true, 'samesite' => 'Strict');
+    session_set_cookie_params($sessionCookie);
+    session_cache_limiter('no-cache');
+
+    session_start();
+
+    $timeNow = time();
+
+    if ($reset) {
+        // focus reset session
+        session_unset();
+        $_SESSION['timefinish'] = $timeNow + 300;
+        $_SESSION['valid'] = false;
+        session_regenerate_id();
+        $_SESSION['valid'] = true;
+        $_SESSION['timeout'] = $timeNow + $lifeTime;
+        unset($_SESSION['timefinish']);
+        return true;
+    } else {
+        // normal start
+        if (isset($_SESSION['valid'], $_SESSION['timeout'])) {
+            // already have session
+            if ($_SESSION['valid']) {
+                // valid should be true
+                if ($_SESSION['timeout'] < $timeNow) {
+                    // old session is used, must reject
+                    header('location:/');
+                    session_destroy();
+                    exit;
+                } elseif ($_SESSION['timeout'] - $lifeTime * 0.9 < $timeNow) {
+                    // session has been used for some time, so need be refresh
+                    $_SESSION['timefinish'] = $timeNow + 300;
+                    $_SESSION['valid'] = false;
+                    session_regenerate_id();
+                    $_SESSION['valid'] = true;
+                    $_SESSION['timeout'] = $timeNow + $lifeTime;
+                    unset($_SESSION['timefinish']);
+                    return true;
+                } else {
+                    // session still has long life time, use it directly
+                    return true;
+                }
+            } else {
+                if ($_SESSION['timefinish'] >= $timeNow) {
+                    // refresh within 5min, if the network is unstable, old session needs to be refreshed again
+                    $_SESSION['timefinish'] -= 200;
+                    $_SESSION['valid'] = false;
+                    session_regenerate_id();
+                    $_SESSION['valid'] = true;
+                    $_SESSION['timeout'] = $timeNow + $lifeTime;
+                    unset($_SESSION['timefinish']);
+                    return true;
+                } else {
+                    // old session is used, must reject
+                    header('location:/');
+                    session_destroy();
+                    exit;
+                }
+            }
+        } else {
+            // not have session
+            session_unset();
+            $_SESSION['timeout'] = $timeNow + $lifeTime;
+            $_SESSION['valid'] = true;
+            return true;
+        }
+    }
+}
