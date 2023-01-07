ad215e881df3b73efd78eed8b1a233b987a1e30b
ZhongWwwHhh <79801664+ZhongWwwHhh@users.noreply.github.com>
2023-01-07 21:27:32 +0800

    minify quit

--- account/quit/quit.php
+++ account/quit/quit.php
@@ -1,11 +1,8 @@
 <?php
 	session_start();
-	$_SESSION = array();
 	if(isset($_COOKIE[session_name()])){
 		setcookie(session_name(), '', time()-3600, '/');
 	}
 	session_destroy();
-	header('refresh:1;url = /');
-    echo "跳转至主页...";
-
+	header('location:/');
 ?>
