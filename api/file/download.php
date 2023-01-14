55762f0d003890e3d0579c0da239ac6cbdbdbd22
ZhongWwwHhh <79801664+ZhongWwwHhh@users.noreply.github.com>
2023-01-14 13:54:54 +0800

    add echo for readile

--- api/file/download.php
+++ api/file/download.php
@@ -28,5 +28,5 @@ if (!file_exists($download_path . $filename)) {
     header('Accept-Ranges:bytes');
     header('Content-Disposition:attachment;filename=' . str_replace('+', ' ', urlencode($filename)));
     header('Content-Length:' . filesize($download_path . $filename));
-    readfile($download_path . $filename);
+    echo readfile($download_path . $filename);
 }
