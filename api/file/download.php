7abe025b6cc02ef2451d8b096e2024ddc4dd4d37
ZhongWwwHhh <79801664+ZhongWwwHhh@users.noreply.github.com>
2023-03-29 23:09:50 +0800

    改进大文件下载

--- api/file/download.php
+++ api/file/download.php
@@ -18,15 +18,31 @@ checkStr($_GET['filepath']) || bad();
 $filepath = $_GET['filepath'];
 $filename = $_GET['filename'];
 $download_path = "../../../storage/$filepath/";
+$fullpath = $download_path . $filename;
 
-if (!file_exists($download_path . $filename)) {
+if (!file_exists($fullpath)) {
     bad();
 } else {
+    $chunksize = 5 * (1024 * 1024);
+    $size = filesize($fullpath);
+
     header('Content-Type:application/octet-stream;charset=utf8');
     // cache 2 days
     header('cache-control:public,max-age=172800');
     header('Accept-Ranges:bytes');
     header('Content-Disposition:attachment;filename=' . str_replace('+', ' ', urlencode($filename)));
-    header('Content-Length:' . filesize($download_path . $filename));
-    readfile($download_path . $filename);
+    header('Content-Length:' . $size);
+
+    if ($size > $chunksize) {
+        $handle = fopen($fullpath, "rb");
+        while (!feof($handle)) {
+            print(@fread($handle, $chunksize));
+            ob_flush();
+            flush();
+        }
+        fclose($handle);
+    } else {
+        readfile($fullpath);
+    }
+    exit;
 }
